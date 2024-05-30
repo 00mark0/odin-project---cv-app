0a4a0bfe83756d6f02a10fe54837828cb7c4a250
00mark0 <00marko.r@gmail.com>
2024-05-30 18:29:39 +0200

    adjusting the print function

--- src/App.jsx
+++ src/App.jsx
@@ -19,34 +19,34 @@ function App() {
   const printDocument = () => {
     setIsPrinting(true);
     setTimeout(() => {
-      const input1 = document.getElementById("printable-area-1");
-      const input2 = document.getElementById("printable-area-2");
+      const printableArea = document.getElementById("printable-area-1");
 
-      const pdf = new jsPDF("p", "mm", "a4");
-      const pdfHeightMM = pdf.internal.pageSize.getHeight();
-
-      html2canvas(input1, { scale: 2 }).then((canvas1) => {
-        const imgData1 = canvas1.toDataURL("image/png");
-        const imgProps1 = pdf.getImageProperties(imgData1);
+      html2canvas(printableArea, { scale: 1 }).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("p", "mm", "a4");
         const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight1 = (imgProps1.height * pdfWidth) / imgProps1.width;
-        pdf.addImage(imgData1, "PNG", 0, 0, pdfWidth, pdfHeight1);
+        const pdfHeightMM = pdf.internal.pageSize.getHeight();
+        const imgProps = pdf.getImageProperties(imgData);
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        const totalPDFPages = Math.ceil(pdfHeight / pdfHeightMM);
 
-        html2canvas(input2, { scale: 2 }).then((canvas2) => {
-          const imgData2 = canvas2.toDataURL("image/png");
-          const imgProps2 = pdf.getImageProperties(imgData2);
-          const pdfHeight2 = (imgProps2.height * pdfWidth) / imgProps2.width;
+        for (let i = 0; i < totalPDFPages; i++) {
+          if (i !== 0) pdf.addPage();
+          const srcY = i * pdfHeightMM;
+          pdf.addImage(
+            imgData,
+            "PNG",
+            0,
+            -srcY,
+            pdfWidth,
+            pdfHeight,
+            undefined,
+            "FAST"
+          );
+        }
 
-          if (pdfHeight1 + pdfHeight2 <= pdfHeightMM) {
-            pdf.addImage(imgData2, "PNG", 0, pdfHeight1, pdfWidth, pdfHeight2);
-          } else {
-            pdf.addPage();
-            pdf.addImage(imgData2, "PNG", 0, 0, pdfWidth, pdfHeight2);
-          }
-
-          pdf.save("yourCV.pdf");
-          setIsPrinting(false);
-        });
+        pdf.save("yourCV.pdf");
+        setIsPrinting(false);
       });
     }, 0);
   };
@@ -64,8 +64,6 @@ function App() {
             <Education />
             <Courses />
             <Employments />
-          </div>
-          <div id="printable-area-2" className="mb-6 p-10">
             <Projects />
           </div>
           <button
